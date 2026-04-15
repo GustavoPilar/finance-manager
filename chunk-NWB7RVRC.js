5b5ec50d4a467c48e9589ba5f0835ea3d815d693
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-04-15 11:32:58 -0300

    Auto-generated commit

---
+++ chunk-NWB7RVRC.js
@@ -0,0 +1,1 @@
+import{n as a}from"./chunk-7KKYW6SN.js";import"./chunk-NO6XSEBJ.js";import"./chunk-6IBBTAYW.js";export{a as CrudsModule};
--- chunk-YT4GXBLL.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./chunk-7MXAQYCC.js";import"./chunk-6DEQLNNT.js";import"./chunk-4RNCEF3J.js";export{a as CrudsModule};

f7ce145c09a12b0aca62ef48856568d556d54bc4
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-04-15 12:11:14 -0300

    Fix: Crud Routing AuthGuard

--- src/app/modules/cruds/crud-routing.module.ts
+++ src/app/modules/cruds/crud-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { CrudComponent } from "./crud.component";
 import { CrudListManagerComponent } from "./list/crud-list-manager/crud-list-manager.component";
 import { CrudFormManagerComponent } from "./form/crud-form-manager/crud-form-manager.component";
+import { authGuard } from "../../guards/auth-guard";
 
 export const routes: Routes = [
-  { path: "", component: CrudComponent },
-  { path: "list/:entityName", component: CrudListManagerComponent },
-  { path: "edit/:entityName/:entityId", component: CrudFormManagerComponent },
-  { path: "new/:entityName", component: CrudFormManagerComponent }
+  { path: "", component: CrudComponent, canActivate: [authGuard] },
+  { path: "list/:entityName", component: CrudListManagerComponent, canActivate: [authGuard] },
+  { path: "edit/:entityName/:entityId", component: CrudFormManagerComponent, canActivate: [authGuard] },
+  { path: "new/:entityName", component: CrudFormManagerComponent, canActivate: [authGuard] }
 ]
 
 @NgModule({

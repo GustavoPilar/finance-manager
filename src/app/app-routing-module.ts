7775d79d37cd3c270854996baeda35122e57ac61
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-04-20 10:22:27 -0300

    Fix: remoção de Cadastro das rotas

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,11 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guards/auth-guard';
-import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
   { path: "home", component: HomeComponent, canActivate: [authGuard] },
   { path: "manager", loadChildren: () => import ("./modules/cruds/cruds.module").then(cm => cm.CrudsModule), canActivate: [authGuard] },
   { path: "application", loadChildren: () => import ("./modules/application/application.module").then(cm => cm.ApplicationModule), canActivate: [authGuard] },
